6e614b5552dcf948c4f8694734dc94669cfdace0
Yazhini P <yazhinipalaniappan2004@gmail.com>
2026-04-14 13:41:51 +0530

    Initialize Spring Boot application structure

---
+++ HirehubApplication.java
@@ -0,0 +1,12 @@
+package com.yazhini.hirehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HirehubApplication.class, args);
+    }
+}
